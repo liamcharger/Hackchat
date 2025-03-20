1fe36f601a1a2dd6f41c3cc14ad553edd9e69d3f
Liam Willey <chargerelectronics19@gmail.com>
2025-03-20 12:50:46 -0400

    Initial Commit

---
+++ Hackchat/HackchatApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackchatApp.swift
+//  Hackchat
+//
+//  Created by Liam Willey on 3/20/25.
+//
+
+import SwiftUI
+
+@main
+struct HackchatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HackchatTests/HackchatTests.swift
@@ -0,0 +1,16 @@
+//
+//  HackchatTests.swift
+//  HackchatTests
+//
+//  Created by Liam Willey on 3/20/25.
+//
+
+import Testing
+
+struct HackchatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
